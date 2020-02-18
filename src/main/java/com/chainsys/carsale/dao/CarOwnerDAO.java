3816d3334d01c768ec0e2ea65d6bf9976bc24f84
Muthukrishnan M <varunkrishna9825@gmail.com>
2020-02-18 18:33:50 +0530

    update

--- src/main/java/com/chainsys/carsale/dao/CarOwnerDAO.java
+++ src/main/java/com/chainsys/carsale/dao/CarOwnerDAO.java
@@ -2,17 +2,14 @@ package com.chainsys.carsale.dao;
 
 import java.util.List;
 
-import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
-import org.jdbi.v3.sqlobject.customizer.BindBean;
-import org.jdbi.v3.sqlobject.statement.SqlQuery;
-import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
 
 import com.chainsys.carsale.model.CarOrder;
 import com.chainsys.carsale.model.CarOwner;
 import com.chainsys.carsale.util.DbException;
 
 public interface CarOwnerDAO {
-   	void addCarOwner(@BindBean CarOwner carOwner) throws DbException;
+   	void addCarOwner( CarOwner carOwner) throws DbException;
   void deleteCarDetail(int carOwnerId, int carId) throws DbException;
    public List<CarOwner> viewYourCar(long mobileNo) throws DbException;
   void updateCarPrice(CarOwner carOwner) throws DbException;
